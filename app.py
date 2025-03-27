606d3cb8a32d273599e6848c3ca1519615db74a4
Ikmal Said <ikmalsaid@gmail.com>
2025-03-27 22:24:28 +0800

    Updated app initialization to disable error handling and modified the run configuration to specify host and port for better accessibility.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from main import create_app
 
-app = create_app(demo=True)
+app = create_app(demo=True, err_handler=False)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True, host='0.0.0.0', port=7860)
